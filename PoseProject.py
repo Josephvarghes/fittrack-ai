760efdffd7081838f0820581d9c5e000a3dfa553
Joseph Varghese <josephvarghese98128@gmail.com>
2024-12-31 09:32:29 +0530

    created PoseProject.py

---
+++ PoseProject.py
@@ -0,0 +1,20 @@
+import PoseModule as pm
+import cv2 
+import time 
+
+cap = cv2.VideoCapture('../Videos/1_1.mp4') 
+pTime = 0 
+detector = pm.poseDetector()
+while True: 
+    success, img = cap.read()  
+    img = detector.findPose(img) 
+    lmlist = detector.findPosition(img,draw=False) 
+    if len(lmlist) != 0:
+        print(lmlist[14])
+        cv2.circle(img, (lmlist[14][1],lmlist[14][2]), 15, (255,0,0), cv2.FILLED) 
+    cTime = time.time() 
+    fps = 1/(cTime-pTime) 
+    pTime = cTime 
+    cv2.putText(img, str(int(fps)),(70,50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3) 
+    cv2.imshow("Image", img)  
+    cv2.waitKey(1)

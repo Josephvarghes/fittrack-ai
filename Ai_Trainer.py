c3f7bf13703a483fed9fe0a70849c68ec4c64dfd
Joseph Varghese <josephvarghese98128@gmail.com>
2025-01-03 21:46:11 +0530

    updated

--- Ai_Trainer.py
+++ Ai_Trainer.py
@@ -3,20 +3,26 @@ import numpy as np
 import time  
 import PoseModule as pm
 
-# cap = cv2.VideoCapture("ai-trainer/1_1.mp4") 
+cap = cv2.VideoCapture("ai-trainer/1_1.mp4") 
 detector = pm.poseDetector()
 while True:
-    # success, img = cap.read() 
-    # img = cv2.resize(img, (1280,720))  
-    img = cv2.imread("ai-trainer/test.png")
+    success, img = cap.read() 
+    img = cv2.resize(img, (1280,720))  
+    # img = cv2.imread("ai-trainer/test.png")
 
     img = detector.findPose(img,False) 
     lmlist = detector.findPosition(img,False)
     # print(lmlist) 
 
     if len(lmlist) != 0:
+        # right arm
         detector.findAngle(img,12,14,16)
+
+        # lef arm
+        detector.findAngle(img,11,13,15)
 
 
     cv2.imshow("Image", img) 
     cv2.waitKey(1)
+
+# 27
